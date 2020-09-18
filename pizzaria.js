2d9a4ce8332cce30d2cd9c71c8a0a07714e5e940
Marlon Andrei <marlon@micrologi.com.br>
2020-09-18 19:13:48 -0300

    Update pizzaria.js

--- pizzaria.js
+++ pizzaria.js
@@ -1,6 +1,3 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
 const { ActivityHandler, MessageFactory } = require('botbuilder');
 const { FazerReservaDialog } = require('./componentDialogs/fazerReservaDialog');
 const { LUISRecognizer, LuisRecognizer} = require('botbuilder-ai');
